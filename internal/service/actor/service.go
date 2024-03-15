56afa99b10039c2cb75f5ccf270fef06b4add456
hillow <andrew.hillow@gmail.com>
2024-03-15 15:58:59 +0300

    feat: created actor service base

--- internal/service/actor/service.go
+++ internal/service/actor/service.go
@@ -1,1 +1,27 @@
 package actor
+
+import (
+    "context"
+
+    "main/internal/database"
+)
+
+type IService interface {
+    GetActor(ctx context.Context, id int32) (database.Actor, error)
+    SearchActors(ctx context.Context, id int32) ([]database.Actor, error)
+
+    CreateActor(ctx context.Context, params database.AddActorParams) (int32, error)
+    AddActorToFilm(ctx context.Context, actorId, filmId int32) error
+
+    UpdateActor(ctx context.Context, id int32, film database.OptUpdateActor) error
+
+    DeleteActor(ctx context.Context) error
+}
+
+type Service struct {
+    db *database.Queries
+}
+
+func NewService(db *database.Queries) *Service {
+    return &Service{db: db}
+}
