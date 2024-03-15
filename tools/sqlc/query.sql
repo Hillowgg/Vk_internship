5c2138b3fb9a7dcd6fddf2ff35cf52db5a90aa11
hillow <andrew.hillow@gmail.com>
2024-03-15 03:22:26 +0300

    fix: fixed not parsing

--- tools/sqlc/query.sql
+++ tools/sqlc/query.sql
@@ -48,18 +48,18 @@ WHERE id = $1;
 -- name: SearchFilmsByTitle :many
 SELECT *
 FROM films
-WHERE title LIKE '%$1%';
+WHERE title LIKE '%' || $1 || '%';
 
 -- name: SearchFilmByTitleAndActor :one
 
 WITH ai AS (
-	SELECT id FROM actors where lower(name) LIKE lower('%$2%')
+	SELECT id FROM actors where lower(name) LIKE lower('%' || $2 || '%')
 ), fi AS (
-	SELECT id FROM films WHERE lower(title) LIKE lower('%$1%')
+	SELECT id FROM films WHERE lower(title) LIKE lower('%' || $1 || '%')
 ), f AS (
 	SELECT film_id FROM actors_films WHERE actor_id IN (select * from ai) AND film_id IN (SELECT * FROM fi)
 )
-SELECT (title, description, release_date, rating) FROM f JOIN films ON f.film_id = films.id;
+SELECT title, description, release_date, rating FROM f JOIN films ON f.film_id = films.id;
 
 
 -- name: AddFilm :one
