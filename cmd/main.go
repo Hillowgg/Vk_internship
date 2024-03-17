6ae7edf72189083077840db0588fde8ec834d9b8
hillow <andrew.hillow@gmail.com>
2024-03-17 19:50:39 +0300

    feat: run app

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+    "main/internal/app"
+    "main/internal/logs"
+)
 
+func main() {
+    a, err := app.NewApp()
+    if err != nil {
+        logs.Log.Fatalw("Failed to create app", "err", err)
+    }
+    err = a.Run()
+    if err != nil {
+        logs.Log.Fatalw("Failed to run app", "err", err)
+    }
 }
