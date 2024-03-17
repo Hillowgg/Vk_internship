8492018290347be267774cf25f1a34ce38938a19
hillow <andrew.hillow@gmail.com>
2024-03-17 22:41:38 +0300

    refactor changed id parsing

--- internal/api/film/delete.go
+++ internal/api/film/delete.go
@@ -1,18 +1,16 @@
 package film
 
 import (
-    "encoding/json"
+    "io"
     "net/http"
+
+    "github.com/tidwall/gjson"
 )
 
 func (h *Handler) DeleteFilm(w http.ResponseWriter, r *http.Request) {
-    var filmId int32
-    err := json.NewDecoder(r.Body).Decode(&filmId)
-    if err != nil {
-        http.Error(w, "Failed to decode request", http.StatusBadRequest)
-        return
-    }
-    err = h.serv.DeleteFilm(r.Context(), filmId)
+    body, _ := io.ReadAll(r.Body)
+    filmId := int32(gjson.GetBytes(body, "Id").Int())
+    err := h.serv.DeleteFilm(r.Context(), filmId)
     if err != nil {
         http.Error(w, "Failed to delete film", http.StatusInternalServerError)
         return
