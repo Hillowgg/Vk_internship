6f2e3c21c967acafc5774dedbb56c03af662eb3f
hillow <andrew.hillow@gmail.com>
2024-03-17 19:46:17 +0300

    refactor: logs + mistyping

--- internal/api/film/handler.go
+++ internal/api/film/handler.go
@@ -4,6 +4,7 @@ import (
     "net/http"
 
     "main/internal/api/user"
+    "main/internal/logs"
     "main/internal/service/film"
 )
 
@@ -11,8 +12,9 @@ type Handler struct {
     serv film.IService
 }
 
-func (h *Handler) ServeHttp(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     endpoint := r.URL.Path
+    logs.Log.Infow("Request", "method", r.Method, "endpoint", endpoint)
     switch endpoint {
     case "/film/add":
         user.AdminMiddleware(h.CreateFilm)(w, r)
@@ -20,6 +22,8 @@ func (h *Handler) ServeHttp(w http.ResponseWriter, r *http.Request) {
         user.AdminMiddleware(h.UpdateFilm)(w, r)
     case "/film/delete":
         user.AdminMiddleware(h.DeleteFilm)(w, r)
+    default:
+        w.WriteHeader(http.StatusNotFound)
     }
 }
 
