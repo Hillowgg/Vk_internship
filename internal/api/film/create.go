28449aad86e35f8cbe153a2a7cada8d01f4dac4e
hillow <andrew.hillow@gmail.com>
2024-03-17 19:46:47 +0300

    fix: right validation

--- internal/api/film/create.go
+++ internal/api/film/create.go
@@ -1,22 +1,44 @@
 package film
 
 import (
-    "encoding/json"
+    "errors"
+    "io"
     "net/http"
+    "strconv"
+    "time"
 
+    "github.com/tidwall/gjson"
     "main/internal/logs"
     "main/internal/service/film"
 )
 
-func (h *Handler) CreateFilm(w http.ResponseWriter, r *http.Request) {
-    var f film.NewFilm
-    err := json.NewDecoder(r.Body).Decode(&f)
+func validateNewFilm(bytes []byte) (*film.NewFilm, error) {
+    json := gjson.ParseBytes(bytes)
+    date, err := time.Parse("2006-01-02", json.Get("ReleaseDate").String())
     if err != nil {
-        logs.Log.Errorw("Failed to decode film", "err", err)
-        http.Error(w, "Failed to decode film", http.StatusBadRequest)
+        return nil, err
+    }
+    f := &film.NewFilm{
+        Title:       json.Get("Title").String(),
+        Description: json.Get("Description").String(),
+        ReleaseDate: date,
+        Rating:      int8(json.Get("Rating").Int()),
+    }
+    if f.Title == "" || f.Description == "" || f.Rating < 1 || f.Rating > 10 {
+        return nil, errors.New("invalid film data")
+    }
+    return f, nil
+}
+
+func (h *Handler) CreateFilm(w http.ResponseWriter, r *http.Request) {
+    res, _ := io.ReadAll(r.Body)
+    f, err := validateNewFilm(res)
+    if err != nil {
+        logs.Log.Errorw("Failed to validate film", "film", f, "err", err)
+        http.Error(w, "Failed to validate film", http.StatusBadRequest)
         return
     }
-    _, err = h.serv.CreateFilm(r.Context(), &f)
+    id, err := h.serv.CreateFilm(r.Context(), f)
     if err != nil {
         logs.Log.Errorw("Failed to create film", "film", f, "err", err)
         http.Error(w, "Failed to create film", http.StatusInternalServerError)
@@ -24,4 +46,5 @@ func (h *Handler) CreateFilm(w http.ResponseWriter, r *http.Request) {
     }
     logs.Log.Infow("Added film", "film", f)
     w.WriteHeader(http.StatusOK)
+    w.Write([]byte(strconv.Itoa(int(id))))
 }
